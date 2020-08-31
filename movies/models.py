2e3896a84e5d7d1bc22dc29c21b901878b846505
Amerlan <Amerlan-t@bk.ru>
2020-08-31 21:15:24 +0600

    Add Category, Actor and Genre models. Add movies to installed apps in settings.  Registered created models.Changed timezone

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField('Категория', max_length=150)
+    description = models.TextField('Описание')
+    url = models.SlugField(max_length=150, unique=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+class Actor(models.Model):
+    name = models.CharField('Имя', max_length=150)
+    age = models.PositiveIntegerField('Возраст', default=0)
+    image = models.ImageField('Фото', upload_to='actors/')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Актёр'
+        verbose_name_plural = 'Актёры'
+
+class Genre(models.Model):
+    name = models.CharField('Имя', max_length=100)
+    description = models.TextField('Описание')
+    url = models.SlugField(max_length=150, unique=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Жанр'
+        verbose_name_plural = 'Жанры'
